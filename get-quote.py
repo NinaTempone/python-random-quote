5a8a095142f6b8f62c0a0e2d404fc42e8a0c636c
NinaTempone <70252295+NinaTempone@users.noreply.github.com>
2020-08-26 14:57:56 +1000

    Update get-quote.py

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,11 @@
 def main():
   print("Keep it logically awesome.")
+  
   python3 get-quote.py
   #f = open("quotes.txt")
   #quotes = f.readlines()
   #f.close()
-
+ 
   #print(quotes)
 
 if __name__== "__main__":
